edb372bccb888caee3a630f3639108f275ca8dc7
gaju007-dot <gajanan.kondawar24@mmit.edu.in>
2026-03-13 12:14:39 +0530

    Created using Colab

--- Minimax.py
+++ Minimax.py
@@ -23,7 +23,7 @@
         "colab_type": "text"
       },
       "source": [
-        "<a href=\"https://colab.research.google.com/github/gaju007-dot/Data_Science_Lab_SE_B_35/blob/main/Untitled0.ipynb\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+        "<a href=\"https://colab.research.google.com/github/gaju007-dot/Data_Science_Lab_SE_B_35/blob/main/Minimax.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
       ]
     },
     {
